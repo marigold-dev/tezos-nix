28406a58a663c398aaff6f6260e0f3dd6e60eea2
Ulrik Strid <ulrik.strid@outlook.com>
2022-04-17 14:18:09 +0200

    Try adding declarative builds (#3)
    * Try adding declarative builds
    
    * Add input
    
    * Rename file

---
+++ nix/hydra_spec.nix
@@ -0,0 +1,18 @@
+{ lib }:
+
+{
+  hydra-spec = lib.generators.toJSON { } {
+    "enabled" = 1;
+    "hidden" = false;
+    "description" = "PR \${num}: \${info.title}";
+    "flake_uri" = " 	github:marigold-dev/tezos-nix";
+    "checkinterval" = 30;
+    "schedulingshares" = 100;
+    "enableemail" = false;
+    "emailoverride" = "";
+    "keepnr" = 3;
+    "inputs" = {
+      "pulls" = { "type" = "githubpulls"; "value" = "marigold-dev tezos-nix"; "emailresponsible" = false; };
+    };
+  };
+}
