2a60a7b391a0bfcc7927a1ee32181d8f9634d3be
Ulrik Strid <ulrik.strid@outlook.com>
2023-03-17 17:24:02 +0100

    Try to fix auto-update text (#47)
    * Try to fix auto-update text
    
    * Add debug messages
    
    * better debug message
    
    * Pass require function
    
    * Run nix flake update in action
    
    * More debugging
    
    * match on stderr
    
    * log uri
    
    * Better regex
    
    * Remove error for now

--- .github/auto-update/lib.js
+++ .github/auto-update/lib.js
@@ -1,95 +1,161 @@
-const https = require("https");
-const { URL } = require("url");
+module.exports = (require) => {
+  const https = require("https");
+  const { URL } = require("url");
 
-const { execSync } = require("child_process");
+  const { exec } = require("child_process");
 
-function http_request(uri) {
-  return new Promise((resolve, reject) => {
-    const url = new URL(uri);
+  function http_request(uri) {
+    return new Promise((resolve, reject) => {
+      const url = new URL(uri);
 
-    return https
-      .get(
-        {
-          port: 443,
-          path: url.pathname + url.search,
-          protocol: url.protocol,
-          hostname: url.hostname,
-          method: "GET",
-          headers: {
-            "User-Agent": "GithubActions",
-            Accept: "application/json",
+      console.log("uri: " + uri);
+
+      return https
+        .get(
+          {
+            port: 443,
+            path: url.pathname + url.search,
+            protocol: url.protocol,
+            hostname: url.hostname,
+            method: "GET",
+            headers: {
+              "User-Agent": "GithubActions",
+              Accept: "application/json",
+            },
+            url,
           },
-          url,
-        },
-        (res) => {
-          const response_data = [];
-          res.on("data", (data) => {
-            response_data.push(data);
-          });
+          (res) => {
+            const response_data = [];
+            res.on("data", (data) => {
+              response_data.push(data);
+            });
 
-          res.on("end", () => {
-            const response = Buffer.concat(response_data);
-            const response_string = response.toString();
+            res.on("end", () => {
+              const response = Buffer.concat(response_data);
+              const response_string = response.toString();
 
-            try {
-              return resolve(JSON.parse(response_string));
-            } catch (e) {
-              return reject(e);
-            }
-          });
-        }
-      )
-      .on("error", reject);
-  });
-}
+              console.log("response_string:" + response_string);
 
-function get_commits(sha1, sha2) {
-  return http_request(
-    `https://gitlab.com/api/v4/projects/3836952/repository/compare?from=${sha1}&to=${sha2}`
-  )
-    .then((res) => {
-      return {
-        web_url: res.web_url,
-        commits: res.commits.map((commit) => ({
-          title: commit.title,
-          web_url: commit.web_url,
-        })),
-      };
-    })
-    .catch((e) => {
-      console.error(e);
-      return {
-        error: "Error occurred, there could be relevant commits missing",
-      };
+              try {
+                return resolve(JSON.parse(response_string));
+              } catch (e) {
+                return reject(e);
+              }
+            });
+          }
+        )
+        .on("error", (error) => {
+          console.error(error);
+          reject(error);
+        });
     });
+  }
+
+  function get_commits(sha1, sha2) {
+    return http_request(
+      `https://gitlab.com/api/v4/projects/3836952/repository/compare?from=${sha1}&to=${sha2}`
+    )
+      .then((res) => {
+        return {
+          web_url: res.web_url,
+          commits: res.commits.map((commit) => ({
+            title: commit.title,
+            web_url: commit.web_url,
+          })),
+        };
+      })
+      .catch((e) => {
+        console.error(e);
+        return {
+          error: "Error occurred, there could be relevant commits missing",
+        };
+      });
+  }
+
+  function update_trunk() {
+    return new Promise((resolve, reject) => {
+      console.log("cwd" + process.cwd());
+      exec("nix flake update", (error, output, stderr) => {
+        if (error != null) {
+          console.log("can't update flake");
+          console.error(error);
+          reject(error);
+          return;
+        } else {
+          console.log("output: " + output);
+          console.log("stderr: " + stderr);
+          let prev_sha_regex = /Updated input \'tezos_trunk\':$\n    'gitlab:tezos\/tezos\/([a-z0-9A-Z]+)'/ms;
+          let next_sha_regex = /Updated input \'tezos_trunk\':.*  → 'gitlab:tezos\/tezos\/([a-z0-9A-Z]+)'/ms;
+
+          const prev_match = stderr.match(prev_sha_regex);
+          const next_match = stderr.match(next_sha_regex);
+
+          const prev_sha = prev_match[1];
+          const next_sha = next_match[1];
+
+          // Only write the file if the commit hash has changed
+          // Otherwise just cancel the workflow. We don't need to do anything
+          if (prev_sha.startsWith(next_sha)) {
+            reject(new Error("Shas were the same"));
+            return;
+          }
+
+          resolve({ prev_sha: prev_match[1], next_sha: next_match[1] });
+          return;
+        }
+      });
+    });
+  }
+
+  function find_shas() {
+    return new Promise((resolve, reject) => {
+      exec("git diff ./flake.lock", (error, output) => {
+        if (error != null) {
+          console.log("Can't git diff");
+          console.error(error);
+          reject(error);
+          return;
+        } else {
+          console.log("output: " + output);
+          let prev_sha_regex = /tezos_trunk.*-        "rev": "([A-Za-z0-9]+)"/ms;
+          let next_sha_regex = /tezos_trunk.*\+        "rev": "([A-Za-z0-9]+)"/ms;
+        
+          const prev_match = output.match(prev_sha_regex);
+          const next_match = output.match(next_sha_regex);
+
+          const prev_sha = prev_match[1];
+          const next_sha = next_match[1];
+
+          // Only write the file if the commit hash has changed
+          // Otherwise just cancel the workflow. We don't need to do anything
+          if (prev_sha.startsWith(next_sha)) {
+            reject(new Error("Shas were the same"));
+            return;
+          }
+
+          resolve({ prev_sha, next_sha });
+          return;
+        }      
+      });
+    });
+  }
+
+  function escapeForGHActions(s) {
+    // Escape `"` and `$` characters in a string to work around the following
+    // `` ` `` and `'` seems to be problematic as well
+    // issue:
+    // https://github.com/repo-sync/pull-request/issues/27
+    return s
+      .replace(/\$/g, "\\$")
+      .replace(/"/g, '\\"')
+      .replace(/`/g, "\\`")
+      .replace(/\'/g, "\\'");
+  }
+
+  return {
+    update_trunk,
+    find_shas,
+    get_commits,
+    escapeForGHActions,
+  };
 }
-
-function find_shas() {
-  const output = execSync("git diff ./flake.lock").toString();
-
-  let prev_sha_regex = /tezos_trunk.*-        "rev": "([A-Za-z0-9]+)"/ms;
-  let next_sha_regex = /tezos_trunk.*\+        "rev": "([A-Za-z0-9]+)"/ms;
-
-  const prev_match = output.match(prev_sha_regex);
-  const next_match = output.match(next_sha_regex);
-
-  return { prev_sha: prev_match[1], next_sha: next_match[1] };
-}
-
-function escapeForGHActions(s) {
-  // Escape `"` and `$` characters in a string to work around the following
-  // `` ` `` and `'` seems to be problematic as well
-  // issue:
-  // https://github.com/repo-sync/pull-request/issues/27
-  return s
-    .replace(/\$/g, "\\$")
-    .replace(/"/g, '\\"')
-    .replace(/`/g, "\\`")
-    .replace(/\'/g, "\\'");
-}
-
-module.exports = {
-  find_shas,
-  get_commits,
-  escapeForGHActions,
-};
--- .github/workflows/auto-update.js
+++ .github/workflows/auto-update.js
@@ -1,56 +1,39 @@
 module.exports = async ({ github, context, core, require }) => {
+  const lib = require("./.github/auto-update/lib.js");
   const {
-    find_shas,
+    update_trunk,
     get_commits,
     escapeForGHActions,
-  } = require("./.github/auto-update/lib.js");
-  try {
-    const shas = find_shas();
+  } = lib(require);
 
-    // Only write the file if the commit hash has changed
-    // Otherwise just cancel the workflow. We don't need to do anything
-    if (shas.prev_rev.startsWith(shas.next_sha)) {
-      throw new Error("Shas were the same");
-    }
+  return update_trunk()
+    .then(shas => get_commits(shas.prev_sha, shas.next_sha))
+    .then(diff => {
+      const url = diff.web_url;
 
-    const diff = await get_commits(shas.prev_sha, shas.next_sha);
-
-    const url = diff.web_url;
-
-    const commit_texts = diff.commits.map(({ title, web_url }) => {
-      const message = escapeForGHActions(title);
-      return `* <a href="${web_url}"><pre>${message}</pre></a>`;
-    });
-
-    const post_text = `
-  #### New commits on tezos/tezos Trunk
-  ${commit_texts.join("\n")}
-  
-  #### Diff URL: ${url}
-  
-  ${
-    error != null
-      ? `#### Error
-  
-  ${error}
-  
-  `
-      : ""
-  }
-        `;
-
-    return post_text;
-  } catch (error) {
-    (error) => {
-      console.error(error);
-      core.notice(error.message);
-      github.rest.actions.cancelWorkflowRun({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        run_id: context.runId,
+      const commit_texts = diff.commits.map(({ title, web_url }) => {
+        const message = escapeForGHActions(title);
+        return `* <a href="${web_url}"><pre>${message}</pre></a>`;
       });
 
-      return "";
-    };
-  }
+      const post_text = `
+#### New commits on tezos/tezos Trunk
+${commit_texts.join("\n")}
+
+#### Diff URL: ${url}
+`;
+
+    return post_text;
+  })
+  .catch((error) => {
+    console.error(error);
+    core.notice(error.message);
+    github.rest.actions.cancelWorkflowRun({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      run_id: context.runId,
+    });
+
+    return "";
+  });
 };
