2d4b8ad53f5d60a81b9f8a808e94ae209cfd5ffa
Ulrik Strid <ulrik.strid@outlook.com>
2023-04-13 20:42:46 +0200

    Cheat a bit with nixosModules

--- nix/services/default.nix
+++ nix/services/default.nix
@@ -4,10 +4,15 @@
 }: {
   flake = {
     nixosModules = {
-      tezos-baking = import ./baking.nix {
-        nodePackage = self.packages.octez-node;
-        bakerPackage = self.packages.octez-baker-PtMumbai;
-        accuserPackage = self.packages.octez-accuser-PtMumbai;
+      x86_64-linux_tezos-baking = import ./baking.nix {
+        nodePackage = self.packages.x86_64-linux.octez-node;
+        bakerPackage = self.packages.x86_64-linux.octez-baker-PtMumbai;
+        accuserPackage = self.packages.x86_64-linux.octez-accuser-PtMumbai;
+      };
+      aarch64-linux_tezos-baking = import ./baking.nix {
+        nodePackage = self.packages.aarch64-linux.octez-node;
+        bakerPackage = self.packages.aarch64-linux.octez-baker-PtMumbai;
+        accuserPackage = self.packages.aarch64-linux.octez-accuser-PtMumbai;
       };
     };
   };
