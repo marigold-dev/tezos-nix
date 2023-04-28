f006390ca6d3d8affdbacfab1db40ba43092ae97
Ulrik Strid <ulrik.strid@outlook.com>
2023-04-28 08:08:33 +0200

    nix fmt

--- nix/trunk/overlays.nix
+++ nix/trunk/overlays.nix
@@ -1,11 +1,11 @@
 final: prev: {
   ocaml-ng =
     builtins.mapAttrs
-      (ocamlVersion: curr_ocaml:
-        curr_ocaml.overrideScope' (oself: osuper: {
-          tezos-event-logging-test-helpers = osuper.tezos-event-logging-test-helpers.overrideAttrs (o: rec {
-            propagatedBuildInputs = o.propagatedBuildInputs ++ [ oself.octez-alcotezt ];
-          });
-        }))
-      prev.ocaml-ng;
+    (ocamlVersion: curr_ocaml:
+      curr_ocaml.overrideScope' (oself: osuper: {
+        tezos-event-logging-test-helpers = osuper.tezos-event-logging-test-helpers.overrideAttrs (o: rec {
+          propagatedBuildInputs = o.propagatedBuildInputs ++ [oself.octez-alcotezt];
+        });
+      }))
+    prev.ocaml-ng;
 }
