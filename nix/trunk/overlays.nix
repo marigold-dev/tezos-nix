295864d1f0b5e8213e1e6812bcfcbecebab734e9
Ulrik Strid <ulrik.strid@outlook.com>
2023-04-28 08:19:05 +0200

    Update sources (#92)
    * Update sources
    
    * Fix build names in CI
    
    * Add new dependency to event-logging-test-helpers
    
    * nix fmt

--- nix/trunk/overlays.nix
+++ nix/trunk/overlays.nix
@@ -2,6 +2,10 @@ final: prev: {
   ocaml-ng =
     builtins.mapAttrs
     (ocamlVersion: curr_ocaml:
-      curr_ocaml.overrideScope' (oself: osuper: {}))
+      curr_ocaml.overrideScope' (oself: osuper: {
+        tezos-event-logging-test-helpers = osuper.tezos-event-logging-test-helpers.overrideAttrs (o: rec {
+          propagatedBuildInputs = o.propagatedBuildInputs ++ [oself.octez-alcotezt];
+        });
+      }))
     prev.ocaml-ng;
 }
