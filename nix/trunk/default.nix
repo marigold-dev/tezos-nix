b4fd956ca9e4070438019d21a79e1d0c813d2ebd
Ulrik Strid <ulrik.strid@outlook.com>
2023-05-04 16:39:53 +0200

    Update sources (#98)
    * Update sources
    
    * Fix trunk deps of client
    
    * Fix trunk deps of dac node
    
    * More deps
    
    * nix fmt

--- nix/trunk/default.nix
+++ nix/trunk/default.nix
@@ -5,7 +5,7 @@
 }: let
   overlay = import ./overlays.nix;
   version = {
-    octez_version = "20230502";
+    octez_version = "20230504";
     src = inputs.tezos_trunk;
   };
 in {
--- nix/trunk/overlays.nix
+++ nix/trunk/overlays.nix
@@ -6,6 +6,70 @@ final: prev: {
         tezos-event-logging-test-helpers = osuper.tezos-event-logging-test-helpers.overrideAttrs (o: rec {
           propagatedBuildInputs = o.propagatedBuildInputs ++ [oself.octez-alcotezt];
         });
+
+        tezos-lwt-result-stdlib = osuper.tezos-lwt-result-stdlib.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-error-monad = osuper.tezos-error-monad.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-hacl = osuper.tezos-hacl.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-clic = osuper.tezos-clic.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-signer-backends = osuper.tezos-signer-backends.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-client-base = osuper.tezos-client-base.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-protocol-environment = osuper.tezos-protocol-environment.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-context = osuper.tezos-context.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-version = osuper.tezos-version.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-crypto-dal = osuper.tezos-crypto-dal.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-shell-services = osuper.tezos-shell-services.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-webassembly-interpreter = osuper.tezos-webassembly-interpreter.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-layer2-store = osuper.tezos-layer2-store.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-micheline-rewriting = osuper.tezos-micheline-rewriting.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-proxy-server-config = osuper.tezos-proxy-server-config.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt];
+        });
+
+        tezos-stdlib-unix = osuper.tezos-stdlib-unix.overrideAttrs (o: rec {
+          checkInputs = o.checkInputs ++ [oself.tezt oself.octez-alcotezt oself.tezos-test-helpers];
+        });
       }))
     prev.ocaml-ng;
 }
