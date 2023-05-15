75c091313735d9afcbc4564991d6fdfa9d8f5e1f
Ulrik Strid <ulrik.strid@outlook.com>
2023-05-15 13:51:55 +0200

    Add tezt-ethereum

--- nix/trunk/overlays.nix
+++ nix/trunk/overlays.nix
@@ -5,6 +5,8 @@ final: prev: {
       curr_ocaml.overrideScope' (oself: osuper: {
         # New pacakges
 
+        tezt-ethereum = oself.callPackage ./tezt/tezt-ethereum.nix {};
+
         # Overrides
         tezos-dal-node-lib = osuper.tezos-dal-node-lib.overrideAttrs (o: {
           propagatedBuildInputs = o.propagatedBuildInputs ++ [oself.tezos-gossipsub];
